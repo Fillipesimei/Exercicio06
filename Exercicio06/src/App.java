11b641cda55911b3b5f0e5d8401c7246752f0c81
Lippe_777 <lippesimei@gmail.com>
2026-03-30 20:55:50 -0300

    Exercicio06

---
+++ Exercicio06/src/App.java
@@ -0,0 +1,21 @@
+//Fillipe Símei Souza Ribeiro
+//
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner fs = new Scanner(System.in);
+
+        System.out.println("Digite o número:");
+        int n = fs.nextInt();
+
+        int multiplicador = 1;
+        while (multiplicador <= 10) {
+            System.out.println(n + " x " + multiplicador + " = " + (n * multiplicador));
+            multiplicador++;
+        }
+
+        fs.close();
+    }
+}
